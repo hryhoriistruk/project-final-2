183831e6e05a751bf9d03772ef2a207e3a5f5fae
Hryhorii_Struk <136825012+hryhoriistruk@users.noreply.github.com>
2025-04-30 20:38:24 +0200

    Add files via upload

---
+++ src/test/java/com/javarush/jira/BaseTests.java
@@ -0,0 +1,9 @@
+package com.javarush.jira;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+
+@SpringBootTest
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
+abstract class BaseTests {
+}
